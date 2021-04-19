952e977b064ef310ba8ea8ffcbe260d26091737c
ThanosGi <33462555+ThanosGi@users.noreply.github.com>
2021-04-19 18:57:34 +0300

    import fonts and vectors

--- app/src/main/java/com/unipi/developers/multiplicationlearning/MainActivity.java
+++ app/src/main/java/com/unipi/developers/multiplicationlearning/MainActivity.java
@@ -3,6 +3,7 @@ package com.unipi.developers.multiplicationlearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
