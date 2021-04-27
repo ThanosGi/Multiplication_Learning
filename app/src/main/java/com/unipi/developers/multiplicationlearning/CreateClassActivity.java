49605877fc9165bff1721c7da1c295b319a52196
ThanosGi <33462555+ThanosGi@users.noreply.github.com>
2021-04-27 10:19:39 +0300

    Create Class UI Theory UI MyCanvas created Custom Scroll View created

---
+++ app/src/main/java/com/unipi/developers/multiplicationlearning/CreateClassActivity.java
@@ -0,0 +1,14 @@
+package com.unipi.developers.multiplicationlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_class);
+    }
+}
---
+++ app/src/main/java/com/unipi/developers/multiplicationlearning/TheoryActivity.java
@@ -0,0 +1,40 @@
+package com.unipi.developers.multiplicationlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+public class TheoryActivity extends FullScreen {
+
+    CustomScrollView scrollView;
+    MyCanvas myCanvas1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theory);
+
+        scrollView = findViewById(R.id.scrollview);
+        scrollView.setEnableScrolling(false);
+        myCanvas1 = findViewById(R.id.mycanvas1);
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+}
