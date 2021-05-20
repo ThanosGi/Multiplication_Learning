518ee6d0a1d2d6238331d90e9913fca6a307a48f
ThanosGi <33462555+ThanosGi@users.noreply.github.com>
2021-05-20 12:25:21 +0300

    teacher classes presentation added teacher's students status presentation added

--- app/src/main/java/com/unipi/developers/multiplicationlearning/MyClassesActivity.java
+++ app/src/main/java/com/unipi/developers/multiplicationlearning/MyClassesActivity.java
@@ -1,7 +1,17 @@
 package com.unipi.developers.multiplicationlearning;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -16,11 +26,14 @@ public class MyClassesActivity extends FullScreen {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     ArrayList<String> classesID;
     ArrayList<String> classesNAME;
+    TableLayout tableLayout;
+    TextView class_name_row,class_id_row;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_classes);
         mAuth = FirebaseAuth.getInstance();
+        tableLayout = findViewById(R.id.tableLayout);
     }
 
     @Override
@@ -47,11 +60,32 @@ public class MyClassesActivity extends FullScreen {
                                 }
                             }
 
-                            for (String s : classesNAME) {
-                                Log.e("Name", s);
-                            }
-                            for (String s : classesID) {
-                                Log.e("ID", s);
+                            if (classesNAME.size()==classesID.size()){
+                                for (int i=0; i < classesNAME.size();i++){
+                                    TableRow row= new TableRow(this);
+                                    TableRow.LayoutParams lp;
+                                    lp = new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 4f);
+                                    //row.setLayoutParams(lp);
+                                    class_name_row = new TextView(this);
+                                    class_name_row.setText(classesNAME.get(i));
+                                    class_name_row.setTextColor(Color.rgb(249,195,89));
+                                    class_name_row.setPadding(10,10,10,10);
+                                    class_name_row.setTextSize(20);
+                                    class_name_row.setGravity(Gravity.CENTER_HORIZONTAL);
+                                    class_name_row.setLayoutParams(lp);
+                                    class_id_row = new TextView(this);
+                                    class_id_row.setText(classesID.get(i));
+                                    class_id_row.setTextColor(Color.rgb(249,195,89));
+                                    class_id_row.setPadding(10,10,10,10);
+                                    class_id_row.setTextSize(20);
+                                    class_id_row.setGravity(Gravity.CENTER_HORIZONTAL);
+                                    class_id_row.setLayoutParams(lp);
+                                    row.addView(class_name_row);
+                                    row.addView(class_id_row);
+                                    tableLayout.addView(row);
+                                }
+                            }else {
+                                Toast.makeText(getApplicationContext(),"Wrong Data",Toast.LENGTH_SHORT).show();
                             }
                         }
                     }
--- app/src/main/java/com/unipi/developers/multiplicationlearning/StudentsStatus.java
+++ app/src/main/java/com/unipi/developers/multiplicationlearning/StudentsStatus.java
@@ -1,10 +1,36 @@
 package com.unipi.developers.multiplicationlearning;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -19,6 +45,13 @@ public class StudentsStatus extends FullScreen {
     ArrayList<String> students_progress;
     ArrayList<String> students_username;
     ArrayList<String> students_classId;
+    BarChart barChart;
+    RadarChart radarChart;
+    int current_user = -1;
+    TextView txt_student_name;
+    AppCompatButton btn_prev,btn_next;
+    int zero_lesson,one_lesson,two_lesson,three_lesson,four_lesson,five_lesson,six_lesson,seven_lesson,eight_lesson,nine_lesson,review1,review2,review3,final_review;
+    String json_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +59,39 @@ public class StudentsStatus extends FullScreen {
         setContentView(R.layout.activity_students_status);
 
         mAuth = FirebaseAuth.getInstance();
+        barChart = findViewById(R.id.barChart);
+        radarChart = findViewById(R.id.radarChart);
+        txt_student_name = findViewById(R.id.txt_student_name);
+        btn_next = findViewById(R.id.btn_next);
+        btn_prev = findViewById(R.id.btn_prev);
+
+        btn_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (current_user != -1){
+                    if (current_user > 0){
+                        current_user--;
+                    }else {
+                        current_user = students_username.size()-1;
+                    }
+                    make_chart(current_user);
+                }
+            }
+        });
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (current_user != -1){
+                    if (current_user < students_username.size()-1){
+                        current_user++;
+                    }else {
+                        current_user = 0;
+                    }
+                    make_chart(current_user);
+                }
+            }
+        });
     }
 
     @Override
@@ -37,7 +103,6 @@ public class StudentsStatus extends FullScreen {
         students_username = new ArrayList<>();
         students_classId = new ArrayList<>();
         get_classes();
-
     }
 
     private void get_classes(){
@@ -75,6 +140,8 @@ public class StudentsStatus extends FullScreen {
                             students_classId.add(Objects.requireNonNull(document.get("classId")).toString());
                         }
                     }
+                    current_user = 0;
+                    make_chart(0);
                 });
     }
 
@@ -83,6 +150,80 @@ public class StudentsStatus extends FullScreen {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
             hideSystemUI();
+        }
+    }
+
+    private void make_chart(int pos){
+        txt_student_name.setText(students_username.get(pos));
+        json_data = students_progress.get(pos);
+        JSONObject json;
+        try {
+            json = new JSONObject(json_data);
+            zero_lesson =json.getJSONObject("0").getInt("success");
+            one_lesson =json.getJSONObject("1").getInt("success");
+            two_lesson =json.getJSONObject("2").getInt("success");
+            three_lesson =json.getJSONObject("3").getInt("success");
+            four_lesson =(json.getJSONObject("4").getInt("success"));
+            five_lesson =(json.getJSONObject("5").getInt("success"));
+            six_lesson =(json.getJSONObject("6").getInt("success"));
+            seven_lesson =(json.getJSONObject("7").getInt("success"));
+            eight_lesson =(json.getJSONObject("8").getInt("success"));
+            nine_lesson =(json.getJSONObject("9").getInt("success"));
+            review1 =(json.getJSONObject("test1").getInt("success"));
+            review2 =(json.getJSONObject("test2").getInt("success"));
+            review3 =(json.getJSONObject("test3").getInt("success"));
+            final_review =(json.getJSONObject("finalTest").getInt("success"));
+
+            ArrayList<BarEntry> stats = new ArrayList<>();
+            stats.add(new BarEntry(0,zero_lesson));
+            stats.add(new BarEntry(1,one_lesson));
+            stats.add(new BarEntry(2,two_lesson));
+            stats.add(new BarEntry(3,three_lesson));
+            stats.add(new BarEntry(4,four_lesson));
+            stats.add(new BarEntry(5,five_lesson));
+            stats.add(new BarEntry(6,six_lesson));
+            stats.add(new BarEntry(7,seven_lesson));
+            stats.add(new BarEntry(8,eight_lesson));
+            stats.add(new BarEntry(9,nine_lesson));
+
+            BarDataSet barDataSet = new BarDataSet(stats,getString(R.string.lessons));
+            barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+            barDataSet.setValueTextColor(Color.BLACK);
+            barDataSet.setValueTextSize(16f);
+
+            BarData barData = new BarData(barDataSet);
+
+            barChart.setFitBars(true);
+            barChart.setData(barData);
+            barChart.getDescription().setText(getString(R.string.student_stats));
+            barChart.animateY(2000);
+
+            ArrayList<RadarEntry> reviews = new ArrayList<>();
+            reviews.add(new RadarEntry(review1));
+            reviews.add(new RadarEntry(review2));
+            reviews.add(new RadarEntry(review3));
+            reviews.add(new RadarEntry(final_review));
+
+            RadarDataSet radarDataSet = new RadarDataSet(reviews, getString(R.string.reviews));
+            radarDataSet.setColor(Color.RED);
+            radarDataSet.setLineWidth(2f);
+            radarDataSet.setValueTextColor(Color.RED);
+            radarDataSet.setValueTextSize(14f);
+
+            RadarData radarData = new RadarData();
+            radarData.addDataSet(radarDataSet);
+
+            String[] labels = {getString(R.string.review1),getString(R.string.review2),getString(R.string.review3),getString(R.string.final_review),};
+
+            XAxis xAxis = radarChart.getXAxis();
+            xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+
+            radarChart.clear();
+            radarChart.getDescription().setEnabled(false);
+            radarChart.setData(radarData);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
